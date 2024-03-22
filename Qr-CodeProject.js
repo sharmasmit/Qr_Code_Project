b3506e5e3ab44fa9d8ca3dec7d1c770f2291cc01
sharmasmit <smeet.sharma007@gmail.com>
2024-03-22 11:42:27 +0530

    Some Changes added

--- Qr-CodeProject.js
+++ Qr-CodeProject.js
@@ -1,4 +1,3 @@
-// 1. Use the inquirer npm package to get user input.
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
@@ -27,6 +26,3 @@ inquirer
       // Something else went wrong
     }
   });
-
-// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-// 3. Create a txt file to save the user input using the native fs node module.
